de9eb3a6d9f2b4594fa594ba8452564dd5d2a30a
Kyald <dhikyaldwiansyah@gmail.com>
2025-06-24 14:25:30 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Feature Freeze Game',
+  description: 'Created with v0 by Kyald',
   generator: 'v0.dev',
 }
 
